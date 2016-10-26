26fb27f816d100bf17d99833a9eec439ac3886d5
Administrator <administrator@MacBook-di-Administrator.local>
2016-10-26 16:15:53 +0200

    tmp

--- app/helpers/uploader.rb
+++ app/helpers/uploader.rb
@@ -2,7 +2,7 @@ module Uploader
 
     def self.upload(file)
     	  
-      _f_path = File.join("tmp", file.original_filename)
+      _f_path = File.join("/tmp", file.original_filename)
       File.open(_f_path, "wb") { |f| f.write(IO.read file.tempfile.path) }
       _f_path 
       
