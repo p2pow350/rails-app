d6fae317c0abded685e5a9256dc06dedd33c8dd5
Administrator <administrator@MacBook-di-Administrator.local>
2016-11-15 02:44:08 +0100

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 	
 	def active_class(link_path) current_page?(link_path) ? "active" : "" end
-	
+		
 	# Comparison View
 	def empty_rate(value)
-		value ||= '--'
+		_v = number_with_precision(value, precision: 4)
+		_v ||= '--'
 	end	
-  
+  	
 end
