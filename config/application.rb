d338d943156ca981814617d81da63b48b4bbdc74
Massimo <mvar78@gmail.com>
2017-06-08 13:28:38 +0200

    .

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Gestionale
     config.i18n.fallbacks = [:en]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
     
   end
 end
